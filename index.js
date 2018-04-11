7f64c2e11f50dc3743d64a0a6e7ced982d32ac3b
Adam Barton <adam.barton333@gmail.com>
2018-04-11 03:01:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays(String) {
+ return "Happy holidays!"
+}
+function happyHolidaysTo(string) {
+  return "Happy holidays, you!"
+}
+function happyHolidayTo('Independence Day' 'You!') {
+   return "Happy ${holiday}, ${name)!"
+}
